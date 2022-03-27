11a2a0d4988bf285a7e622e1cce2cd4f342c0fb2
vinc <grantor@outlook.com>
2022-03-27 22:38:45 +0800

    add test func

--- EasyDB_test.go
+++ EasyDB_test.go
@@ -1,19 +1,33 @@
 package easydb
 
 import (
+	"os"
+	"strconv"
 	"testing"
 )
 
+var opt = &Option{
+	Directory:       "./data",
+	DataFileMaxSize: 102400,
+	Enable:          true,
+	Secret:          "1111111111111111",
+}
+
 func TestPut(t *testing.T) {
-	// os.RemoveAll("./data")
-	opt := &Option{
-		Directory:       "./data",
-		DataFileMaxSize: 102400,
-		Enable:          true,
-		Secret:          "1111111111111111",
-	}
+	os.RemoveAll("./data")
+
 	Open(opt)
 	Put([]byte("12343"), []byte("sssss"))
-	Get([]byte("12343"))
 	Close()
+}
+
+func BenchmarkPut(b *testing.B) {
+	Open(opt)
+	defer func() {
+		migrate()
+		Close()
+	}()
+	for i := 0; i < b.N; i++ {
+		Put([]byte(strconv.Itoa(i*1000)), []byte("sssss"))
+	}
 }
